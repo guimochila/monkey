d7fdad9759ed6f0f2f5bfb6a7d8077b9827dbe1b
Guilherme <gui.mochila@gmail.com>
2025-06-03 09:23:19 +0200

    feat(lexer): add keywords and double digit tokens

--- token/token.go
+++ token/token.go
@@ -18,8 +18,17 @@ const (
 	INT   = "INT"
 
 	// Operators.
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT     = "<"
+	GT     = ">"
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// Delimeters.
 	COMMA     = ","
@@ -33,11 +42,21 @@ const (
 	// Keywords.
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
