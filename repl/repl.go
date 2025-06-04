aef3f79959ab255470246ac3db8847381e2a189e
Guilherme <gui.mochila@gmail.com>
2025-06-04 08:14:23 +0200

    feat(repl): add initial repl

---
+++ repl/repl.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2025. guimochila.com. Continuous Learning.
+
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/guimochila/monkey/lexer"
+	"github.com/guimochila/monkey/token"
+)
+
+const Prompt = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, Prompt)
+
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
